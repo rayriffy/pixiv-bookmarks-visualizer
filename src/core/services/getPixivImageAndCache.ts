513d8cdeb4dd01a770c0e3a1072ac00e7c76c398
Phumrapee Limpianchop <git@rayriffy.com>
2022-10-08 11:26:39 +0900

    refactor: preload script, compression

--- src/core/services/getPixivImageAndCache.ts
+++ src/core/services/getPixivImageAndCache.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 import sharp from 'sharp'
+import fetch from 'node-fetch'
 
 const cacheDirectory = path.join(process.cwd(), '.next/cache/pixivProxy')
 
@@ -24,7 +25,8 @@ export const getPixivImageAndCache = async (url: string) => {
 
     const optimizedImage = await sharp(Buffer.from(fetchedImage))
       .webp({
-        quality: 100
+        quality: 85,
+        effort: 6
       })
       .toBuffer()
     fs.writeFileSync(expectedCachePath, Buffer.from(optimizedImage))
---
+++ tools/preloadImage.ts
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+
+import PQueue from 'p-queue'
+
+import { ExtendedPixivIllust } from '../src/core/@types/ExtendedPixivIllust'
+import { getPixivImageAndCache } from '../src/core/services/getPixivImageAndCache'
+
+const bookmarksFilePath = path.join(
+  __dirname,
+  '../.next/cache',
+  'bookmarks.json'
+)
+
+const queue = new PQueue({ concurrency: 20 })
+
+;(async () => {
+  const bookmarks = JSON.parse(
+    fs.readFileSync(bookmarksFilePath, 'utf8')
+  ) as ExtendedPixivIllust[]
+
+  // get all urls
+  const illustUrls = bookmarks.map(bookmark => {
+    const mainUrl = bookmark.image_urls.medium
+    const subUrls = bookmark.meta_pages.slice(1, 3).map(o => o.image_urls.medium)
+
+    return [mainUrl, ...subUrls]
+  }).flat()
+
+  await Promise.all(illustUrls.map(illustUrl => queue.add(async () => {
+    console.log(path.basename(illustUrl))
+    await getPixivImageAndCache(illustUrl)
+  })))
+})()
