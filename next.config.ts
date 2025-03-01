a938a3fcd52f9c4640d1febeb9e22e3db32613ed
Phumrapee Limpianchop <git@rayriffy.com>
2025-03-02 03:22:39 +0700

    chore: staged changes

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next'
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/1',
+      },
+    ]
+  },
+  output: process.env.STANDALONE === 'true' ? 'standalone' : undefined,
+}
+
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig)
